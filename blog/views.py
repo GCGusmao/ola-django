01b0dc4a7db0648f2c9a1e8bcace145a71536178
GCGusmao <69112221+GCGusmao@users.noreply.github.com>
2024-08-02 19:06:49 -0300

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+
+def blog(request):
+    return render(
+       request,
+       'blog/index.html'
+    )
+
+
+def exemplo(request):
+    return render(
+       request,
+       'blog/exemplo.html'
+    )
